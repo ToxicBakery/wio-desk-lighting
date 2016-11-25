770a152d817cdfa1ed83492c4aa5464dabbf6279
Ian Thomas <toxicbakery@gmail.com>
2016-11-25 16:56:47 -0500

    Cleanup of gulp script

--- GulpFile.js
+++ GulpFile.js
@@ -1,29 +1,47 @@
-var gulp = require('gulp');
-var lambda = require('gulp-awslambda');
-var zip = require('gulp-zip');
+const gulp = require('gulp');
+const lambda = require('gulp-awslambda');
+const zip = require('gulp-zip');
+const clean = require('gulp-clean');
+const install = require("gulp-install");
 
-var lambda_params = {
-  FunctionName: 'setLights',
-  Environment: { 
-    "Variables": { 
-      // Access token can be retrieved from the Wio Link app by viewing the API (Menu -> View API)
-      // for your device and copying the access_token from the sample URLs.
-      "ACCESS_TOKEN" : "your-token-here" 
-    }
-  },
-  MemorySize: 128,
-  Runtime: "nodejs4.3",
-  Timeout: 5
+const lambda_params = {
+    FunctionName: 'setLights',
+    Environment: {
+        "Variables": {
+            // Access token can be retrieved from the Wio Link app by viewing the API (Menu -> View API)
+            // for your device and copying the access_token from the sample URLs.
+            "ACCESS_TOKEN": "your-token-here"
+        }
+    },
+    MemorySize: 128,
+    Runtime: "nodejs4.3",
+    Timeout: 5
 };
 
-var opts = {
-  region: 'us-west-2'
+const opts = {
+    region: 'us-west-2'
 };
 
-gulp.task('default', function() {
-  return gulp.src('dest/**/*')
-            .pipe(zip('archive.zip'))
-            .pipe(lambda(lambda_params, opts))
-            .pipe(gulp.dest('.'));
+gulp.task('clean', function () {
+    return gulp.src('dest/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copy', ['clean'], function () {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dest/'));
+});
+
+gulp.task('npmInstall', ['copy'], function () {
+    return gulp.src('dest/')
+        .pipe(gulp.dest('./'))
+        .pipe(install({production: true}));
+});
+
+gulp.task('default', ['npmInstall'], function () {
+    return gulp.src('dest/**/*')
+        .pipe(zip('archive.zip'))
+        .pipe(lambda(lambda_params, opts))
+        .pipe(gulp.dest('.'));
 });
 
